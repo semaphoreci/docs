9fe8260b7812dbb5062f8f3926ab03e08dc1ca36
Milana <milana.stojadinov@gmail.com>
2018-07-12 04:21:17 +0200

    Add ability to update a doc page

---
+++ deploy_docs.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require "net/http"
+require "json"
+
+class UpdateDocs
+
+  CONTENT_TYPE_JSON = "application/json"
+  BASE_URL = "docsapi.helpscout.net"
+  PORT = 443
+  ARTICLES_URL = "/v1/articles/"
+  API_KEY = ENV['DOCS_API_KEY']
+
+  def initialize
+    @http = Net::HTTP.new(BASE_URL, PORT)
+    @http.use_ssl = true
+  end
+
+  def put(article_id, text)
+    request = Net::HTTP::Put.new(path(article_id))
+    request.basic_auth API_KEY, "X"
+
+    request.content_type = CONTENT_TYPE_JSON
+    request.body = JSON.generate({ "text": text })
+    trigger(request)
+  end
+
+  private
+
+  def trigger(request)
+    request['Connection'] = 'keep-alive'
+    response = @http.request(request)
+  end
+
+  def path(article_id)
+    [ARTICLES_URL,article_id].join
+  end
+end
