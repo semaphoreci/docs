d739e356e1db4c95d9bf5e773007ac5f4d66fc83
Darko Fabijan <darko@renderedtext.com>
2019-12-23 15:36:34 +0100

    🐷 hot-fix for logo link

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var logo = document.getElementsByClassName('md-logo')[0];
+
+  logo.setAttribute('href', "http://docs.semaphoreci.com");
+});
