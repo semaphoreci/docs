a415421a13c73d3d7321d1ce8f9e9dd0e9bba13d
tomas f <46322567+TomFern@users.noreply.github.com>
2024-06-02 15:21:19 -0300

    Updated pipelines, added short video to reorder blocks

--- docs/src/components/VideoTutorial/index.js
+++ docs/src/components/VideoTutorial/index.js
@@ -7,9 +7,9 @@ import Youtube from '@site/static/img/youtube.svg';
  * @param {string} url - the "embed mode" youtube url
  * @returns 
  */
-const VideoTutorial  = ({title, src}) => {
+const VideoTutorial  = ({title, src, opened}) => {
     return (
-        <details className={styles.details}> 
+        <details className={styles.details} {...(opened && { open: true })}>  
          <summary className={styles.summary}><span className={styles.icon}><Youtube/></span>Video Tutorial: {title}</summary>
          <div className={styles.content}>
            <iframe className={styles.iframe} width="560" height="315" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
